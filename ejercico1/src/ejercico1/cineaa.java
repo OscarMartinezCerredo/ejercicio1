6972d74f659198ba40da08e44117fcc13bd01afc
Cobra <155960539+OscarMartinezCerredo@users.noreply.github.com>
2024-01-09 12:10:01 +0100

    esta terminado

--- ejercico1/src/ejercico1/cineaa.java
+++ ejercico1/src/ejercico1/cineaa.java
@@ -1,10 +1,55 @@
 package ejercico1;
 
+import java.util.Scanner;
+
 public class cineaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner =new Scanner(System.in);
+    char  [][] silla=new char[10][10];
+  
+    int fila;
+    int con;
+   
+     for (int i =0; i<10;i++) {
+    	 
+    	 for (int j=0;j<10;j++){
+      silla[i][j]='L';
+    	 }
+     }
+     for(int i=0;i<10;i++) {
+    	 
+    	 for(int j=0;j<10;j++) {
+    		 System.out.print(silla[i][j] + " ");
+    		 
+    		 }
+    	 System.out.println("");
+    		 
+    	 }
+     do {
+       System.out.println("dime que fila quieres");
+       fila= scanner.nextInt();
+       System.out.println("dime la comuna");
+       con = scanner.nextInt();
+       if(silla[fila][con]=='L') {
+       silla[fila][con]='O';
+       }
+       else {
+    	  System.out.println("ocupado");
+       }
+	for(int i=0;i<10;i++) {
+   	 
+   	 for(int j=0;j<10;j++) {
+   		 System.out.print(silla[i][j] + " ");
+   	 }
+   	 System.out.println("");
 	}
+     }while(con!=-1);
+	
+	
+   		 }
+     }
+	
 
-}
+
